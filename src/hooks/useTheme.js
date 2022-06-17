41403f027efb960ac61b0c261230da05af8dc5b3
misathemeb <melissabridi@gmail.com>
2022-06-17 12:34:40 -0400

    initial commit

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const onToggleTheme = () => {
+    setTheme((previousTheme) => (previousTheme === "light" ? "dark" : "light"));
+  };
+  const isDarkTheme = theme === "dark";
+
+  return { onToggleTheme, isDarkTheme };
+};
+
+export default useTheme;
